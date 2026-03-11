14c555f47207255687683ff4c0e6a4339679d48e
Ken Chan <chan141993@gmail.com>
2026-03-11 23:36:31 +0000

    comment

--- casinoData.js
+++ casinoData.js
@@ -1,20 +1,20 @@
 // All casino review pages - add new casinos here
 const casinoPages = [
-	{ title: 'Luna Casino', url: '/reviews/lunacasino.html', rating: 5 },
-	{ title: 'BOYLE Casino', url: '/reviews/boylecasino.html', rating: 5 },
-	{ title: 'PlayOJO', url: '/reviews/playojo.html', rating: 5 },
-	{ title: 'Swift Casino', url: '/reviews/swiftcasino.html', rating: 5 },
-	{ title: 'BestOdds', url: '/reviews/bestodds.html', rating: 5 },
-	{ title: 'NetBet', url: '/reviews/netbet.html', rating: 5 },
-	{ title: 'BresBet', url: '/reviews/bresbet.html', rating: 4.5 },
-	{ title: 'BetTOM', url: '/reviews/bettom.html', rating: 4.5 },
-	{ title: 'LiveScore Bet', url: '/reviews/livescorebet.html', rating: 4.5 },
-	{ title: 'Kwiff', url: '/reviews/kwiff.html', rating: 4.5 },
-	// { title: 'Ladbrokes', url: '/reviews/ladbrokes.html', rating: 5 },
-	// { title: 'Gala Bingo', url: '/reviews/galabingo.html', rating: 4.5 },
-	// { title: 'Gala Casino', url: '/reviews/galacasino.html', rating: 4 },
+	{ title: 'Luna Casino', url: '/reviews/lunacasino', rating: 5 },
+	{ title: 'BOYLE Casino', url: '/reviews/boylecasino', rating: 5 },
+	{ title: 'PlayOJO', url: '/reviews/playojo', rating: 5 },
+	{ title: 'Swift Casino', url: '/reviews/swiftcasino', rating: 5 },
+	{ title: 'BestOdds', url: '/reviews/bestodds', rating: 5 },
+	{ title: 'NetBet', url: '/reviews/netbet', rating: 5 },
+	{ title: 'BresBet', url: '/reviews/bresbet', rating: 4.5 },
+	{ title: 'BetTOM', url: '/reviews/bettom', rating: 4.5 },
+	{ title: 'LiveScore Bet', url: '/reviews/livescorebet', rating: 4.5 },
+	{ title: 'Kwiff', url: '/reviews/kwiff', rating: 4.5 },
+	// { title: 'Ladbrokes', url: '/reviews/ladbrokes', rating: 5 },
+	// { title: 'Gala Bingo', url: '/reviews/galabingo', rating: 4.5 },
+	// { title: 'Gala Casino', url: '/reviews/galacasino', rating: 4 },
 	// Add new casino reviews like this:
-	// { title: "New Casino Name", url: "/reviews/new-casino.html", rating: 4.5 }
+	// { title: "New Casino Name", url: "/reviews/new-casino", rating: 4.5 }
 ];
 
 // Get total
